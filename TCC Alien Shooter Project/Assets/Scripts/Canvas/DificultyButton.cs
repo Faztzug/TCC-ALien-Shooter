5e834ed976f69b0634a8a55138c9518d758fc401
Faztz <faztzug@gmail.com>
2023-11-19 01:51:50 -0300

    Atualização Seleção Fases
    Agora possuem imagens unicas para cada fase
    Fix botão de dificuldade perdendo cor de selecionado
    Fix Fase 3 como nome errado na tela de vitoria

--- TCC Alien Shooter Project/Assets/Scripts/Canvas/DificultyButton.cs
+++ TCC Alien Shooter Project/Assets/Scripts/Canvas/DificultyButton.cs
@@ -3,18 +3,32 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.ProBuilder;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
 public class DificultyButton : ButtonSound
 {
     [SerializeField] private GameDificulty gameDificulty = GameDificulty.Normal;
+    private Color normalColor;
+    private Color selectedColor;
+
     protected override void Start()
     {
         base.Start();
         if(GameState.SaveData.gameDificulty == gameDificulty) button.Select();
+        normalColor = button.colors.normalColor;
+        selectedColor = button.colors.selectedColor;
     }
     void OnEnable()
     {
         if(button != null & GameState.SaveData.gameDificulty == gameDificulty) button.Select();
+    }
+
+    private void Update()
+    {
+        if (EventSystem.current.currentSelectedGameObject == button.gameObject) return;
+        var colorBlock = button.colors;
+        colorBlock.normalColor = GameState.SaveData.gameDificulty == gameDificulty ? selectedColor : normalColor;
+        button.colors = colorBlock;
     }
 
     public void SetDificulty()
--- TCC Alien Shooter Project/Assets/Scripts/Canvas/LevelButton.cs
+++ TCC Alien Shooter Project/Assets/Scripts/Canvas/LevelButton.cs
@@ -1,11 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class LevelButton : ButtonSound
 {
     [SerializeField] private int levelIndex;
     private MenuController menuController;
+    [SerializeField] private Sprite levelImageOn;
+    [SerializeField] private Sprite levelImageOff;
+    [SerializeField] private Image image;
 
     protected override void Start()
     {
@@ -17,10 +21,16 @@ public class LevelButton : ButtonSound
     public void UpdateState()
     {
         button.enabled = (GameState.SaveData.unlockLevelsTo >= levelIndex || GameState.GodMode);
+        image.sprite = button.enabled ? levelImageOn : levelImageOff;
     }
 
     public void LoadLevelByIndex()
     {
         menuController.LoadLevel(levelIndex);
+    }
+
+    private void OnValidate()
+    {
+        if(image != null) image.sprite = levelImageOn;
     }
 }
